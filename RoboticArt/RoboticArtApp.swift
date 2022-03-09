49c63a092764412274a79776ba532e1d27ddd931
Martin Maly <martinmaly66@hotmail.com>
2022-03-08 19:04:49 -0500

    Initial Commit

---
+++ RoboticArt/RoboticArtApp.swift
@@ -0,0 +1,17 @@
+//
+//  RoboticArtApp.swift
+//  RoboticArt
+//
+//  Created by Martin Maly on 2022-03-08.
+//
+
+import SwiftUI
+
+@main
+struct RoboticArtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
