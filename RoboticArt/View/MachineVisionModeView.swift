18d89566f628068aa2c666433360c26f77314960
Martin Maly <martinmaly66@hotmail.com>
2022-03-24 01:48:07 -0400

    Check if any of the other fingers have a finger down

--- RoboticArt/View/MachineVisionModeView.swift
+++ RoboticArt/View/MachineVisionModeView.swift
@@ -50,13 +50,97 @@ struct MachineVisionModeView: View {
             perform: { _ in
                 var notesToPlay = [NoteType]()
                 
+                //MARK: - left hand
+                if let tip =  machineVisionViewModel.leftThumbFingerLocations?.first,
+                   let bottom = machineVisionViewModel.leftThumbFingerLocations?.last {
+                    
+                    //AND tip is over any key, then add that key to notesToPlay
+                    if tip.y > bottom.y, let note = getNote(for: tip)  {
+                        notesToPlay.append(note)
+                    }
+                }
+                
                 //left index
                 if let tip =  machineVisionViewModel.leftIndexFingerLocations?.first,
                    let bottom = machineVisionViewModel.leftIndexFingerLocations?.last {
                     
                     //AND tip is over any key, then add that key to notesToPlay
-                    if tip.y > bottom.y  {
-                        notesToPlay.append(getNote(for: tip))
+                    if tip.y > bottom.y, let note = getNote(for: tip)  {
+                        notesToPlay.append(note)
+                    }
+                }
+                
+                if let tip =  machineVisionViewModel.leftMiddleFingerLocations?.first,
+                   let bottom = machineVisionViewModel.leftMiddleFingerLocations?.last {
+                    
+                    //AND tip is over any key, then add that key to notesToPlay
+                    if tip.y > bottom.y, let note = getNote(for: tip)  {
+                        notesToPlay.append(note)
+                    }
+                }
+                
+                if let tip =  machineVisionViewModel.leftRingFingerLocations?.first,
+                   let bottom = machineVisionViewModel.leftRingFingerLocations?.last {
+                    
+                    //AND tip is over any key, then add that key to notesToPlay
+                    if tip.y > bottom.y, let note = getNote(for: tip)  {
+                        notesToPlay.append(note)
+                    }
+                }
+                
+                if let tip =  machineVisionViewModel.leftLittleFingerLocations?.first,
+                   let bottom = machineVisionViewModel.leftLittleFingerLocations?.last {
+                    
+                    //AND tip is over any key, then add that key to notesToPlay
+                    if tip.y > bottom.y, let note = getNote(for: tip)  {
+                        notesToPlay.append(note)
+                    }
+                }
+                
+                //MARK: - right hand
+                if let tip =  machineVisionViewModel.rightThumbFingerLocations?.first,
+                   let bottom = machineVisionViewModel.rightThumbFingerLocations?.last {
+                    
+                    //AND tip is over any key, then add that key to notesToPlay
+                    if tip.y > bottom.y, let note = getNote(for: tip)  {
+                        notesToPlay.append(note)
+                    }
+                }
+                
+                //left index
+                if let tip =  machineVisionViewModel.rightIndexFingerLocations?.first,
+                   let bottom = machineVisionViewModel.rightIndexFingerLocations?.last {
+                    
+                    //AND tip is over any key, then add that key to notesToPlay
+                    if tip.y > bottom.y, let note = getNote(for: tip)  {
+                        notesToPlay.append(note)
+                    }
+                }
+                
+                if let tip =  machineVisionViewModel.rightMiddleFingerLocations?.first,
+                   let bottom = machineVisionViewModel.rightMiddleFingerLocations?.last {
+                    
+                    //AND tip is over any key, then add that key to notesToPlay
+                    if tip.y > bottom.y, let note = getNote(for: tip)  {
+                        notesToPlay.append(note)
+                    }
+                }
+                
+                if let tip =  machineVisionViewModel.rightRingFingerLocations?.first,
+                   let bottom = machineVisionViewModel.rightRingFingerLocations?.last {
+                    
+                    //AND tip is over any key, then add that key to notesToPlay
+                    if tip.y > bottom.y, let note = getNote(for: tip)  {
+                        notesToPlay.append(note)
+                    }
+                }
+                
+                if let tip =  machineVisionViewModel.rightLittleFingerLocations?.first,
+                   let bottom = machineVisionViewModel.rightLittleFingerLocations?.last {
+                    
+                    //AND tip is over any key, then add that key to notesToPlay
+                    if tip.y > bottom.y, let note = getNote(for: tip)  {
+                        notesToPlay.append(note)
                     }
                 }
                 
@@ -66,7 +150,9 @@ struct MachineVisionModeView: View {
         .environmentObject(machineVisionViewModel)
     }
     
-    func getNote(for point: CGPoint) -> NoteType {
+    func getNote(for point: CGPoint) -> NoteType? {
+        
+        return nil
         
         return NoteType.d1
     }
